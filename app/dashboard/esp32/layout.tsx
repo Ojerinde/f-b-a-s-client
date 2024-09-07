2e6cc37978cace5ab4b014fa8bb59301f1851fab
Ojerinde <joelojerinde@gmail.com>
2024-09-07 12:42:25 +0100

    feat: update email logic

--- app/dashboard/esp32/layout.tsx
+++ app/dashboard/esp32/layout.tsx
@@ -27,6 +27,10 @@ export default function Layout({
       const deviceData = GetItemFromLocalStorage("deviceData");
 
       if (!deviceData || !deviceData.email || !deviceData.deviceLocation) {
+        emitToastMessage(
+          "Device location not found. Please go to the settings page to set up the location of the device to communicate with.",
+          "error"
+        );
         return;
       }
 
@@ -51,34 +55,34 @@ export default function Layout({
     }
   }, []);
 
-  // useEffect(() => {
-  //   const socket = getWebSocket();
+  useEffect(() => {
+    const socket = getWebSocket();
 
-  //   const handleEsp32Feedback = (event: MessageEvent) => {
-  //     const data = JSON.parse(event.data);
+    const handleEsp32Feedback = (event: MessageEvent) => {
+      const data = JSON.parse(event.data);
 
-  //     if (data.event !== "esp32_data_feedback") return;
+      if (data.event !== "esp32_data_feedback") return;
 
-  //     setIsFetchingEsp32details(false);
-  //     if (data.payload.error) {
-  //       emitToastMessage("Failed to fetch device data", "error");
-  //     } else {
-  //       emitToastMessage("Device data fetched successfully", "success");
-  //       dispatch(AddEsp32Details(data.payload.data));
-  //     }
-  //   };
+      setIsFetchingEsp32details(false);
+      if (data.payload.error) {
+        emitToastMessage("Failed to fetch device data", "error");
+      } else {
+        emitToastMessage("Device data fetched successfully", "success");
+        dispatch(AddEsp32Details(data.payload.data));
+      }
+    };
 
-  //   if (socket) {
-  //     socket.addEventListener("message", handleEsp32Feedback);
-  //   }
+    if (socket) {
+      socket.addEventListener("message", handleEsp32Feedback);
+    }
 
-  //   // Clean up event listener when component unmounts
-  //   return () => {
-  //     if (socket) {
-  //       socket.removeEventListener("message", handleEsp32Feedback);
-  //     }
-  //   };
-  // }, []);
+    // Clean up event listener when component unmounts
+    return () => {
+      if (socket) {
+        socket.removeEventListener("message", handleEsp32Feedback);
+      }
+    };
+  }, []);
 
   return (
     <>
